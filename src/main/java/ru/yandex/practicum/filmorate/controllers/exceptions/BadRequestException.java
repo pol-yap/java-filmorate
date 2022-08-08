3f88edc86d7b8fb9bbd2bcfcb24000b3ea22fb75
Pol <a.poltavski@yandex.ru>
2022-08-08 15:16:52 +0300

    fix: учитываем замечания ревьюера 2

--- src/main/java/ru/yandex/practicum/filmorate/controllers/exceptions/BadRequestException.java
+++ src/main/java/ru/yandex/practicum/filmorate/controllers/exceptions/BadRequestException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.controllers.exceptions;
 
-public class BadRequestException extends RuntimeException{
+public class BadRequestException extends RuntimeException {
     public BadRequestException(String message) {
         super(message);
     }
