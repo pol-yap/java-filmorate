73f834d23db5f353829437c98b9ce1f905d4f732
Pol <a.poltavski@yandex.ru>
2022-09-12 20:38:04 +0300

    fix: убрать из REST API изменеие списка жанров и рейтингов

--- src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/GenreController.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Genre;
 import ru.yandex.practicum.filmorate.service.GenreService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,16 +21,5 @@ public class GenreController {
     @GetMapping("/{id}")
     public Genre findById(@PathVariable int id) {
         return service.findById(id);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Genre create(@Valid @RequestBody Genre genre) {
-        return service.create(genre);
-    }
-
-    @PutMapping
-    public Genre update(@Valid @RequestBody Genre genre) {
-        return service.update(genre);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaaController.java
@@ -1,12 +1,10 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Mpaa;
 import ru.yandex.practicum.filmorate.service.MpaaService;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -23,16 +21,5 @@ public class MpaaController {
     @GetMapping("/{id}")
     public Mpaa findById(@PathVariable int id) {
         return service.findById(id);
-    }
-
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Mpaa create(@Valid @RequestBody Mpaa mpa) {
-        return service.create(mpa);
-    }
-
-    @PutMapping
-    public Mpaa update(@Valid @RequestBody Mpaa mpa) {
-        return service.update(mpa);
     }
 }
